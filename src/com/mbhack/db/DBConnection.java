1c2b155455228e11555cc4728688584b28592423
Swapnil Daingade <swapnil.daingade@gmail.com>
2016-11-06 00:17:02 -0700

    Consumer accepting parking spot working!

--- src/com/mbhack/db/DBConnection.java
+++ src/com/mbhack/db/DBConnection.java
@@ -42,19 +42,15 @@ public class DBConnection {
 		}
 	  }
 		
-	  public void executeQuery(String query) {
-			
-    	}
-		
 		// values("user1", "House No 1, City test, CA", "111-111-1111", "suv", false, true, false, 1, True);
-		public void insertProducer(ProducerPayload pp, String placeId){
+		public void insertProducer(ProducerPayload pp, String placeId) throws Exception {
 			try {
 			//String query = "insert into test(name, address, phone, parking_type, is_charging, is_available," +  
 			//		"is_handicapped, producer_id, need_rfid) values (" + pp.getName() + ", " + pp.getAddress() + ", " + pp.getPhone() +
 			//		", " + pp.getParkingType() + ", " + pp.getIsCharging() + ", " + pp.getIsAvailable() + ", " + pp.getIsHandicapped() +
 			//		", " + pp.getProducerId() + ", " + pp.getNeedRfid() + ")";
 				
-			String query = "insert into test(name, address, phone, parking_type, is_charging, is_available," +  
+			String query = "insert into producer(name, address, phone, parking_type, is_charging, is_available," +  
 					"is_handicapped, producer_id, need_rfid, lat, longi, place_id) values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
 			
 			PreparedStatement preparedStmt = conn.prepareStatement(query);
@@ -75,29 +71,63 @@ public class DBConnection {
 			} catch (Exception e) {
 				System.out.println("insertProducer - QueryFailed!");
 				e.printStackTrace();
-				
+				throw e;
 			}
 				
 		}
 		
-		public boolean selectSpotQuery(ConsumerPayload cp) {
+		public boolean selectSpotQuery(ConsumerPayload cp) throws Exception {
 			
 			try {
-				String query = "select * from test where is_available=true and place_id=?";
+				String query = "select * from producer where is_available=true and place_id=?";
 				PreparedStatement preparedStmt = conn.prepareStatement(query);
 				preparedStmt.setString(1, cp.getPlaceId());
 				if (preparedStmt.execute()) {
 					ResultSet rs = preparedStmt.getResultSet();
-					if (rs.first()) return true;
-					else return false;
-				} else return false;
+					if (rs.first()) {
+						System.out.println("selectSpotQuery - Parking spot found.");
+						return true;
+					} else {
+						System.out.println("selectSpotQuery - Parking spot not found");
+						return false;
+					}
+				} else {
+					System.out.println("selectSpotQuery - Parking spot not found");
+					return false;
+				}
 				
-			} catch (SQLException e) {
+			} catch (Exception e) {
 				System.out.println("selectSpotQuery - QueryFailed!");
 				e.printStackTrace();
+				throw e;
 			}
-			return false;
 		}
 
+		public boolean makeUnavailable(ConsumerPayload cp) throws Exception {
+			try {
 
+				String query = "update producer SET is_available=false where place_id=?";
+				PreparedStatement preparedStmt = conn.prepareStatement(query);
+				preparedStmt.setString(1, cp.getPlaceId());
+				if (!preparedStmt.execute()) {
+					if (preparedStmt.getUpdateCount() > 0) {
+						System.out.println("makeUnavailable - Parking spot made unavailable");
+						return true;
+					}
+					else {
+						System.out.println("makeUnavailable - Parking spot could not be made unavailable 1");
+						return false;
+					}
+				} else {
+					System.out.println("makeUnavailable - Parking spot could not be made unavailable 2");
+					return false;
+				}
+				
+			} catch (Exception e) {
+				System.out.println("makeUnavailable - QueryFailed!");
+				e.printStackTrace();
+				throw e;
+			}
+			
+		}
 }
--- src/com/mbhack/resources/ParkingConsumer.java
+++ src/com/mbhack/resources/ParkingConsumer.java
@@ -1,5 +1,14 @@
 package com.mbhack.resources;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.URL;
+import java.net.URLConnection;
+
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -9,6 +18,9 @@ import javax.ws.rs.core.Response;
 
 import com.mbhack.db.DBConnection;
 import com.mbhack.payload.ConsumerPayload;
+
+import se.walkercrou.places.GooglePlaces;
+import se.walkercrou.places.PlaceBuilder;
 
 @Path("/consumer")
 public class ParkingConsumer {
@@ -23,12 +35,56 @@ public class ParkingConsumer {
 	    try {
 	       System.out.println("Got Add request. consumerId: " + cp.getConsumerId() + ", placeId: " + cp.getPlaceId());
 	  	   DBConnection conn = DBConnection.getConnection();
-    	   if (!conn.selectSpotQuery(cp))
+    	   if (!conn.selectSpotQuery(cp)) {
+    		   System.out.println("ParkingConsumer.add - Could not select parking spot.");
     		   return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(STATUS_ERROR).build();
+    	   }
+    	   
+    	   //update producer table to false
+    	   if (!conn.makeUnavailable(cp)) {
+    		   System.out.println("ParkingConsumer.add - Could not make spot unavailable.");
+    		   return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(STATUS_ERROR).build();
+    	   }
+    	   //Delete from google
+    	   //GooglePlaces client = new GooglePlaces(ParkingService.API_KEY);
+    	   //Place place = new PlaceBuilder(name, lat, lng, types)
+    	   //client.deletePlace(place, extraParams);
+    	   deletePlace(cp.getPlaceId());
 
 	       return Response.status(Response.Status.OK).entity(STATUS_OK).build();
 	    }catch (Exception e) {
 	       throw new WebApplicationException(Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(e.getMessage()).build());
 	    } 
+	  }
+	  
+	  private void deletePlace(String placeId) {
+		  try {
+		  String payload = "{ \"place_id\" : \"" + placeId + "\"}";
+
+		  URL url = new URL("https://maps.googleapis.com/maps/api/place/delete/json?key=" + ParkingService.API_KEY);
+		  URLConnection connection = url.openConnection();
+		  connection.setDoInput(true);
+		  connection.setDoOutput(true);
+
+		  connection.connect();
+
+		  OutputStream os = connection.getOutputStream();
+		  PrintWriter pw = new PrintWriter(new OutputStreamWriter(os));
+		  pw.write(payload);
+		  pw.close();
+
+		  InputStream is = connection.getInputStream();
+		  BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+		  String line = null;
+		  StringBuffer sb = new StringBuffer();
+		  while ((line = reader.readLine()) != null) {
+		      sb.append(line);
+		  }
+		  is.close();
+		  String response = sb.toString();
+		  System.out.println(response);
+		  } catch (Exception e) {
+			  System.out.println("deletePlace - Failed to delete place from Google.");
+		  }
 	  }
 }
